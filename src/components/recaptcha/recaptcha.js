7b85a72fccf885bcffeb75b0d5ff46a1c6bcde64
lallysmbc <stephen.lally.smbc@gmail.com>
2020-10-21 12:34:52 +0100

    feat(recaptcha): js module for accessability

---
+++ src/components/recaptcha/recaptcha.js
@@ -0,0 +1,21 @@
+function Recaptcha ($module) {
+  this.$module = $module
+}
+
+/**
+* Update recaptcha html element with accessability attributes
+*/
+Recaptcha.prototype.updateAttributes = function () {
+    const textarea = document.getElementById('g-recaptcha-response')
+    const iFrame = document.getElementsByTagName("iframe")
+    textarea.setAttribute("aria-label", "recaptcha");
+    textarea.setAttribute("aria-disabled", "true");
+    iFrame[0].setAttribute("title", "recaptcha");
+    iFrame[1].setAttribute("title", "recaptcha");
+}
+
+Recaptcha.prototype.init = function () {
+    window.addEventListener('load', this.updateAttributes.bind(this));
+}
+
+export default Recaptcha
