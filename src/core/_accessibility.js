fd36959cedf6906c05f4d8133fc6baeaf71956b1
Stuart Wilkes <stuart.wilkes@stockport.gov.uk>
2021-01-27 08:20:48 +0000

    feat(booking_with_time_nojs): clean up js

--- src/core/_accessibility.js
+++ src/core/_accessibility.js
@@ -1,13 +1,13 @@
 // Only current option available for this Accessibility Core Module is to
 // show elements that should remain hidden when JavaScript is disabled
 function Accessibility ($module) {
-    this.$module = $module
+  this.$module = $module
 }
 
 Accessibility.prototype.init = function () {
   // Check if it has a hidden modifier, to remove as we have JS enabled
-  if (this.$module.classList.contains('smbc-accessibility__nojs--hidden')){
-      this.$module.classList.remove('smbc-accessibility__nojs--hidden')
+  if (this.$module.classList.contains('smbc-accessibility__nojs--hidden')) {
+    this.$module.classList.remove('smbc-accessibility__nojs--hidden')
   }
 }
 
